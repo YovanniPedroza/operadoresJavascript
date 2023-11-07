5b16565d4bd5d3b3d2b3ec59aa400fc2e4b5cfb0
Yovanni Pedroza <yovannipedroza@gmail.com>
2023-11-07 09:35:05 -0500

    Se termina archivo con información solicitada

---
+++ operadoresJavascript.js
@@ -0,0 +1,49 @@
+// Operadores de Asignación //
+
+let total;
+total = 100;
+total += 50;
+total -= 25;
+total *= 2;
+total /= 5;
+console.log(total);  // Total = 50
+
+// Operadores de Comparación //
+
+let numeroA = 10;
+let numeroB = 20;
+let sonIguales = numeroA == numeroB;
+let sonDiferentes = numeroA != numeroB;
+let sonIgualesEnValoryTipo= numeroA === numeroB;
+let sonDiferentesEnValoryTipo = numeroA !== numeroB;
+console.log (sonIguales);
+console.log (sonDiferentes)
+console.log (sonIgualesEnValoryTipo);
+console.log (sonDiferentesEnValoryTipo);
+
+
+// Operadores Lógicos //
+
+let condicionA = true;
+let condicionB = false;
+let ambasTrue = condicionA && condicionB;
+let algunaTrue = condicionA || condicionB;
+let condicionANegada = !condicionA;
+console.log (ambasTrue);
+console.log (algunaTrue);
+console.log (condicionANegada);
+
+// Operadores Matemáticos //
+
+let valorA = 15;
+let valorB = 5;
+let suma = valorA + valorB;
+let resta = valorA - valorB;
+let multiplicacion = valorA * valorB;
+let division = valorA / valorB;
+let resto = valorA % valorB;
+console.log (suma);
+console.log (resta);
+console.log (multiplicacion);
+console.log (division);
+console.log (resto);
